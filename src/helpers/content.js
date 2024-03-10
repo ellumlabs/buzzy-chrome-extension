5b6996a55fc1b5ecc79ca3b3a9c8b3b4ffa9061a
Vlad M <vladmrnv@gmail.com>
2024-03-10 00:23:37 -0800

    Update content.js

--- src/helpers/content.js
+++ src/helpers/content.js
@@ -1,6 +1,6 @@
 let imageDropEnabled = false;
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "triggerBuzz") {
     changeCursor();
     imageDropEnabled = true;
