dee1e0e603291b5dcdd751c9bc797ef9e94ff325
Vlad M <vladmrnv@gmail.com>
2024-03-10 00:01:28 -0800

    moved logic to content

--- src/helpers/background.js
+++ src/helpers/background.js
@@ -1,47 +1,7 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === "triggerBuzz") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: main,
-      });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "triggerBuzz" });
     });
   }
 });
-
-const main = () => {
-  const originalCursorStyle = document.documentElement.style.cursor;
-
-  let originalCursor = true;
-
-  const triggerBuzz = () => {
-    // console.log("background");
-    if (originalCursor) {
-      originalCursor = false;
-      console.log("Trigger Buzz");
-
-      const customCursorURL = chrome.runtime.getURL("images/customCursor.svg");
-      document.documentElement.style.cursor = `url('${customCursorURL}') 15 15, auto`;
-
-      document.addEventListener("click", handleMouseClickElementSelect);
-    }
-  };
-
-  const handleMouseClickElementSelect = (e) => {
-    // console.log("mouse clicked");
-    document.documentElement.style.cursor = originalCursorStyle;
-    let clickedElement = e.target;
-
-    console.log(clickedElement);
-    if (clickedElement != null) {
-      chrome.action.onClicked.addListener((tab) => {
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ["content.js"],
-        });
-      });
-    }
-  };
-
-  triggerBuzz();
-};
--- src/helpers/content.js
+++ src/helpers/content.js
@@ -1,19 +1,49 @@
-// document.addEventListener('click', function(event) {
-//     event.preventDefault();
+let imageDropEnabled = false;
 
-//     var clickedElement = event.target;
-
-//     var imgElement = document.createElement('img');
-//     imgElement.src = 'https://example.com/path/to/your/image.png'; // URL of the image you want to drop
-//     imgElement.style.position = 'absolute';
-//     imgElement.style.left = event.pageX + 'px';
-//     imgElement.style.top = event.pageY + 'px';
-//     imgElement.style.zIndex = '1000';
-
-//     document.body.appendChild(imgElement);
-//   }, false);
-
-document.addEventListener("click", function (event) {
-    
-  console.log("content.js", event);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("req", request);
+  if (request.action === "triggerBuzz") {
+    console.log("content triggererd");
+    changeCursor();
+    imageDropEnabled = true;
+  }
 });
+
+document.addEventListener(
+  "click",
+  (e) => {
+    if (imageDropEnabled) {
+      e.preventDefault();
+      dropImageAtClick(e.clientX, e.clientY);
+      resetCursor();
+      imageDropEnabled = false;
+    }
+  },
+  true
+);
+
+const dropImageAtClick = (x, y) => {
+  console.log("dropImage");
+  const img = document.createElement("img");
+  img.src = chrome.runtime.getURL("images/customCursor.svg");
+  img.style.position = "fixed";
+  img.style.left = `${x}px`;
+  img.style.top = `${y}px`;
+  img.style.transform = "translate(-50%, -50%)";
+  document.body.appendChild(img);
+};
+
+const changeCursor = () => {
+  const customCursorURL = chrome.runtime.getURL("images/customCursor.svg");
+  document.documentElement.style.cursor = `url('${customCursorURL}') 15 15, auto`;
+
+  Array.from(document.querySelectorAll("*")).forEach((el) => {
+    el.style.cursor = `url('${customCursorURL}') 15 15, auto`;
+  });
+};
+
+const resetCursor = () => {
+  Array.from(document.querySelectorAll("*")).forEach((el) => {
+    el.style.cursor = "auto";
+  });
+};
